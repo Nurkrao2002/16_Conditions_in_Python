08ebec0a523252e57d83464788e46481e24c7348
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-17 00:25:54 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,29 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(5):
+#     print(i,end='')
+
+# for i in range(5):
+#     print(i,end=' ')
+    
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(4,5):
+#     print(i)
+
+# for i in range(1,20,1):
+#     print(i)
+
+# for i in range(1,20,2):
+#     print(i)
+
+# for i in range(0,20,2):
+#     print(i)
+
+# for i in range(20,1,-1):
+#     print(i)
+
+# for i in range(10,1,-2):
+#     print(i)
