8e4c5cfc8196a31653fba771751e2bf1efbc9f00
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-17 00:27:29 +0530

    Update Bytearray.py

--- Bytearray.py
+++ Bytearray.py
@@ -1,34 +1,35 @@
-# a=b'hi'
-# print(type(a))
-# print(bytearray(a))
+a=b'hi'
+print(type(a))
+print(bytearray(a))
 
-# y=b"python"
-# print(y,type(y))
-# print(list(y))
-# print(tuple(y))
-# print(set(y))
+y=b"python"
+print(y,type(y))
+print(list(y))
+print(tuple(y))
+print(set(y))
 
-# y=b"python"
-# for i in y:
-#     print(i)
+y=b"python"
+for i in y:
+    print(i)
 
-# print(ord('Y'))
-# print(chr(80))
+print(ord('Y'))
+print(chr(80))
 
 
-# y=b"python"
-# print(type(y))
-# y[0]=90 # no modifications
+y=b"python"
+print(type(y))
+y[0]=90 # no modifications
 
-# v=b'[1,2,3]'
-# print(list(v))
+v=b'[1,2,3]'
+print(list(v))
 
-# print(ord('['))
-# print(ord(','))
-# print(chr(80))
+print(ord('['))
+print(ord(','))
+print(chr(80))
 
-# m=bytearray(b"python")
-# print(m,type(m))
-# print(m[0])
-# m[0]=80
-# print(m)
+m=bytearray(b"python")
+print(m,type(m))
+print(m[0])
+m[0]=80
+print(m)
+
