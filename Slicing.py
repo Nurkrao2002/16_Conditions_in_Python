d95f6febfe1da75349732e3ad0a3526520d889b3
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-17 00:28:06 +0530

    Update Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -1,38 +1,38 @@
-# c='python'
-# print(c[:])
-# print(c[0:])
-# print(c[1:])
-# print(c[1:5])
-# print(c[2:3])
-# print(c[3:3])
-# print(c[4:5])
+c='python'
+print(c[:])
+print(c[0:])
+print(c[1:])
+print(c[1:5])
+print(c[2:3])
+print(c[3:3])
+print(c[4:5])
 
-# c='python'
-# print(c[-1])
-# print(c[-4])
-# print(c[-6])
-# print(c[-8])
+c='python'
+print(c[-1])
+print(c[-4])
+print(c[-6])
+print(c[-8])
 
-# c='python'
-# print(c[-1:])
-# print(c[-2:])
-# print(c[-6:])
+c='python'
+print(c[-1:])
+print(c[-2:])
+print(c[-6:])
 
-# c='python'
-# print(c[-1:-2])
-# print(c[-5:-2])
+c='python'
+print(c[-1:-2])
+print(c[-5:-2])
 
-# c='python'
-# print(c[::])
-# print(c[::1])
-# print(c[0::1])
-# print(c[0::2])
-# print(c[1::2])
-# print(c[2::2])
+c='python'
+print(c[::])
+print(c[::1])
+print(c[0::1])
+print(c[0::2])
+print(c[1::2])
+print(c[2::2])
 
-# c='python'
-# print(c[::-1])
-# print(c[::-2])
-# print(c[::-3])
-# v='madam'
-# print(v[::-1])
+c='python'
+print(c[::-1])
+print(c[::-2])
+print(c[::-3])
+v='madam'
+print(v[::-1])
